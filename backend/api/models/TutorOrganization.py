aee30b58f9b539b74c56bfb4bfbc1d6c7084a609
jaredmc32 <no>
2022-11-13 11:25:57 -0600

    fixed minor warnings that popped up

--- backend/api/models/TutorOrganization.py
+++ backend/api/models/TutorOrganization.py
@@ -10,7 +10,7 @@ class TutorOrganization(models.Model):
 
   tutOrgID = models.PositiveSmallIntegerField(primary_key=True)
   #MANDATORY M-M relation with Tutor
-  tutor = models.ManyToManyField(Tutor, blank=False, null=False)
+  tutor = models.ManyToManyField(Tutor, blank=False)
   # MANDATORY M-M relation with TutorOrgManager
-  tutOrgMan = models.ManyToManyField(TutorOrgManager, blank=False, null=False)
+  tutOrgMan = models.ManyToManyField(TutorOrgManager, blank=False)
   tutOrgName = models.CharField(unique=True, max_length=30)

9ab6f33267636f5123fd65b946c06657f20f3aae
Jabari <jabari.r.washington@gmail.com>
2022-12-04 13:06:22 -0600

    Registering user

--- backend/api/Fuser.py
+++ backend/api/Fuser.py
@@ -15,7 +15,10 @@ class Fuser():
     if request.method=='POST':
       email=request.POST['email']
       pswd=request.POST['password']
-      CUser.login(email,pswd)
+      if CUser.login(email,pswd)==True:
+        return render(request,'homeuser.html',{})
+      else:
+        return render(request,'login.html',{'message':"incorrect login Try again"})
       
     # need to add authenticate once entered in information in login area
     # getting intial page layout completed
@@ -26,5 +29,23 @@ class Fuser():
   
   def searchtutorsession(request):
     return render(request,'' ,{})
+  
+  def createProfile(request):
+    if request.method=='POST' and request.POST['name']==None:
+      first=request.POST['Fname']
+      last=request.POST['Lname']
+      email=request.POST['email']
+      phone=request.POST['PhoneNumber']
+      pswd=request.POST['password']
+      pswd2=request.POST['password']
+      if User.checkpassword(pswd,pswd2):
+        User.register(first,last,email,phone,pswd,pswd2)
+      else:
+        render(request,'login.html',{'message2':"Incorrect Password"})
+        
+      
+      render(request, )
+    else:
+      return render(request,'createProfile.html',{})
   
   

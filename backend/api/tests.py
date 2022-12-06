2c971e98762fd723b435c0b9bcffb3266950b6ac
fernando <bigoshpayan@gmail.com>
2022-12-06 15:51:51 -0600

    Finished Student and TutOrgMan tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -2,64 +2,147 @@ from django.test import TestCase
 
 # Create your tests here.
 from api.models.Student import Student
+from api.models.TutorOrganization import TutorOrganization
+from api.models.TutorOrgManager import TutorOrgManager
 from api.models.TutorSession import TutorSession
-from api.models.Review import Review
+from api.models.Class import Class
+from api.models.Tutor import Tutor
+from api.models.SessionResource import SessionResource
 from django.test import RequestFactory, TestCase
-from api.Fstudent import Fstudent
+from api.FtutorOrgManager import FtutorOrgManager
+from api.FtutorOrg import FtutorOrg
 from api.management.commands.setup_test_data import Command
 from django.core.management import call_command
 
 
-class testUser(TestCase):
+class testStudent(TestCase):
   fixtures = ['fixture.json']
 
   def setUp(self):
     self.factory = RequestFactory()
   
-  def test_rateTutorSession(self):
-    # Create an instance of a GET request.
-    session = TutorSession.objects.all()[0]
-    
-    student = session.student.all()[0]
-    user = student.user
-    email_address = user.email_address
+  def test_createTutorSession(self):
+    # Tutor Organization
+    org = TutorOrganization.objects.all()[0]
+    orgName = org.tutOrgName
 
-    request = self.factory.post('sendrating',
+    # The Request we're making
+    request = self.factory.post('createSession',
       {
-        'session': session.tutorSessID,
-        'rating': 5,
-        'email': email_address
+        'name': orgName,
+        'date': "2022-12-25T15:31:12",
+        'SessName': "Fernando's Comp Sci Tutoring Session"
       }
     )
 
-    review = Review.objects.filter(
-      tutSess=session.pk
-    ).filter(
-      student=student.pk
-    ).first()
+    response = FtutorOrg.createSession(request)
+    self.assertEquals(response.status_code, 200)
 
-    if review:
-      self.assertTrue(True)
-    else:
-      self.fail()
-    
-    response = Fstudent.rate(request)
-    self.assertEqual(response.status_code, 200)
+  def test_insertClassResource(self):
+    # Tutor Session
+    tutSes = TutorSession.objects.all()[0]
+    session = tutSes.sessName
 
-  def test_EXAMPLE(self):
+    request = self.factory.post('insertResource',
+      {
+        'className': "IT 326",
+        'classData': "Slides 10_15",
+        'sessName': session
+      }
+    )
+
+    response = FtutorOrg.insertResource(request)
+    self.assertEquals(response.status_code, 200)
+
+  def test_removeTutorSession(self):
+    # Tutor Session
+    session = TutorSession.objects.all()[0]
+    sess = session.sessName
+
+    request = self.factory.post('removeSession',
+      {
+        'SessName': session
+      }
+    )
+
+    response = FtutorOrg.removeSession(request)
+    self.assertEquals(response.status_code, 200)
+
+  def test_removeUserFromSession(self):
     # Session
     session = TutorSession.objects.all()[0]
+    sess = session.sessName
 
     # Student
     student = session.student.all()[0]
     user = student.user
     email_address = user.email_address
 
-    # The Request we're making
-    request = self.factory.post('testing',
+
+    request = self.factory.post('removeuser',
       {
-        'session': session.tutorSessID,
-        'rating': 5,
+        'name': sess,
         'email': email_address
       }
     )
+    
+
+    response = FtutorOrgManager.removeUser(request)
+    self.assertEquals(response.status_code, 200)
+
+  def test_addClassToSession(self):
+    # Session
+    session = TutorSession.objects.all()[0]
+    sess = session.sessName
+
+    request = self.factory.post('addClassToSession',
+      {
+        'className': "IT 326",
+        'classDesc': "TutorTim Rocks!",
+        'sessName': sess
+      }
+    )
+
+    response = FtutorOrg.addClasstoSession(request)
+    self.assertEquals(response.status_code, 200)
+
+
+  def test_addTutorToSession(self):
+    # Tutor
+    tutor = Tutor.objects.all()[0]
+    user = tutor.user
+    email_address = user.email_address
+
+    # Session
+    session = TutorSession.objects.all()[0]
+    sess = session.sessName
+
+    request = self.factory.post('addTutorToSession',
+      {
+        'tutorEmail': email_address,
+        'sessName': sess,
+      }
+    )
+
+    response = FtutorOrg.addTutorToSession(request)
+    self.assertEquals(response.status_code, 200)
+
+  def test_createTutorOrg(self):
+    # Tutor Org Manager
+    tutorOrgManager = TutorOrgManager.objects.all()[0]
+    user = tutorOrgManager.user
+
+    # Variables we are passing
+    email_address = user.email_address
+
+    # The Request we're making
+    request = self.factory.post('createTutorOrg',
+      {
+        'email': email_address,
+        'name': "The Tutor Org for Comp Sci",
+        'desc': "We teach Java, Python, and C++!"
+      }
+    )
+
+    response = FtutorOrgManager.createTutorOrg(request)
+    self.assertEquals(response.status_code, 200)
